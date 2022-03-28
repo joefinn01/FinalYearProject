f471abe11638892be495e601c0f239671e691428
Joe Finn <joefinn01@hotmail.com>
2022-03-28 11:07:22 +0100

    Adding drag float 4 imgui helper function

--- FYP/FYP/Helpers/ImGuiHelper.cpp
+++ FYP/FYP/Helpers/ImGuiHelper.cpp
@@ -91,6 +91,77 @@ bool ImGuiHelper::DragFloat3(const std::string& label, DirectX::XMFLOAT3& values
 	return bUpdated;
 }
 
+bool ImGuiHelper::DragFloat4(const std::string& label, DirectX::XMFLOAT4& values, const float& columnWidth, const float& speed, const float& min, const float& max)
+{
+	ImGuiIO& io = ImGui::GetIO();
+
+	bool bUpdated = false;
+
+	ImGui::PushID(label.c_str());
+
+	ImGui::Columns(2);
+	ImGui::SetColumnWidth(0, columnWidth);
+	ImGui::Text(label.c_str());
+	ImGui::NextColumn();
+
+	ImGui::PushMultiItemsWidths(4, ImGui::CalcItemWidth());
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+	float lineHeight = io.Fonts->Fonts[0]->FontSize + ImGui::GetStyle().FramePadding.y * 2.0f;
+	ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+	ImGui::Button("X", buttonSize);
+	ImGui::SameLine();
+
+	if (ImGui::DragFloat("##X", &values.x, speed, min, max, "%.2f") == true)
+	{
+		bUpdated = true;
+	}
+
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+
+	ImGui::Button("Y", buttonSize);
+	ImGui::SameLine();
+
+	if (ImGui::DragFloat("##Y", &values.y, speed, min, max, "%.2f") == true)
+	{
+		bUpdated = true;
+	}
+
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+
+	ImGui::Button("Z", buttonSize);
+	ImGui::SameLine();
+
+	if (ImGui::DragFloat("##Z", &values.z, speed, min, max, "%.2f") == true)
+	{
+		bUpdated = true;
+	}
+
+	ImGui::PopItemWidth();
+	ImGui::SameLine();
+
+	ImGui::Button("A", buttonSize);
+	ImGui::SameLine();
+
+	if (ImGui::DragFloat("##A", &values.w, speed, min, max, "%.2f") == true)
+	{
+		bUpdated = true;
+	}
+
+	ImGui::PopItemWidth();
+
+	ImGui::PopStyleVar();
+
+	ImGui::Columns(1);
+
+	ImGui::PopID();
+
+	return bUpdated;
+}
+
 void ImGuiHelper::Text(const std::string& label, const char* format, float columnWidth, ...)
 {
 	bool bUpdated = false;
--- FYP/FYP/Helpers/ImGuiHelper.h
+++ FYP/FYP/Helpers/ImGuiHelper.h
@@ -8,6 +8,7 @@ class ImGuiHelper
 public:
 	static bool DragFloat(const std::string& label, float& value, const float& columnWidth = 100.0f, const float& speed = 0.1f, const float& min = 0, const float& max = 0);
 	static bool DragFloat3(const std::string& label, DirectX::XMFLOAT3& values, const float& columnWidth = 100.0f, const float& speed = 0.1f, const float& min = 0, const float& max = 0);
+	static bool DragFloat4(const std::string& label, DirectX::XMFLOAT4& values, const float& columnWidth = 100.0f, const float& speed = 0.1f, const float& min = 0, const float& max = 0);
 
 	static void Text(const std::string& label, const char* format, float columnWidth = 100.0f, ...);
 
